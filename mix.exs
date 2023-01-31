00bd1d6e30adc0623a5177e374c8083bb7d0ec43
Kanak Bhuva <kanakbhuvagit@gmail.com>
2023-01-31 10:31:23 +0530

    upgrade bamboo version

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule PasswordGenerator.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:httpoison, "~> 2.0.0"},
-      {:bamboo, "~> 1.7.1"}
+      {:bamboo, "~> 2.3.0"},
+      {:bamboo_phoenix, "~> 1.0"}
     ]
   end
 
